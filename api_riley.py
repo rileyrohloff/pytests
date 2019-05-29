94dba93fb3145f9ed13b3f26299525587dfb1e35
rileyrohloff <rileyrohloff@workfront.com>
2019-05-29 08:07:34 -0600

    added some new tests for tasks and also a test for logging out using the original session

--- api_riley.py
+++ api_riley.py
@@ -19,26 +19,56 @@ def create_session():
     return login.status_code
 
 def create_proj(name, session):
-
     params = {
     'name':name,
     'status':'PLN',
     'groupID':'5b9534860397593c6ee5fdbe2b578ede'
     }
 
-    create_proj = req.post(f'https://rileyrohloff.my.workfront.com/attask/api/v10.0/project?updates={params}&sessionID={session}')
+    auth = {
+    'sessionID':session
+    }
+    create_proj = req.post(f'https://rileyrohloff.my.workfront.com/attask/api/v10.0/project?updates={params}', headers=auth)
     response = create_proj.json()
     global proj_ID
     proj_ID = response['data']['ID']
     proj_name = response['data']['name']
     return proj_name
 
-def update_proj(ID, session):
+def create_task(name, session, proj):
+    params = {
+    'name':name,
+    'projectID':proj,
+    'status':'INP'
+    }
 
+    headers = {
+    'sessionID':session
+    }
+
+    task_create = req.post(utilites.create_task_url, params=params, headers=headers)
+    response = task_create.json()
+    global taskID
+    taskID = response['data']['ID']
+    task_name = response['data']['name']
+    return task_name
+def update_task(ID, session):
+
+    params = {
+    'status':'CPL'
+    }
+    headers = {
+    'sessionID':session
+    }
+
+    update_task_put = req.put(utilites.task_url + ID, headers=headers, params=params)
+    response = update_task_put.json()
+    return response['data']['status']
+
+def update_proj(ID, session):
     update = {
     'name':'Update Test'
     }
-
     update_post = req.put(f"https://rileyrohloff.my.workfront.com/attask/api/v10.0/project/{ID}?updates={update}&sessionID={session}")
     response = update_post.json()
     proj_name = response['data']['name']
@@ -49,3 +79,11 @@ def tear_down(ID, session_3):
     response = delete_proj.json()
     print(response)
     return delete_proj.status_code
+
+def closeTest(session):
+    session_auth = {
+    'sessionID':session
+    }
+    logout_req = req.get(utilites.logout_url, headers=session_auth)
+    response = logout_req.json()
+    return response['data']
--- utilites.py
+++ utilites.py
@@ -16,3 +16,6 @@ create_proj_url = 'http://rileyrohloff.my.workfront.com/attask/api/v10.0/project
 delete_proj_url = f'http://domain/attask/api/v10.0/%s/%s'
 delete_postbin_url = 'http://postbin.a-us-dev.wfk8s.com/postbins/%s'
 login_url = f'https://rileyrohloff.my.workfront.com/attask/api/v10.0/login?username={user}&password={password_user}'
+logout_url = f"https://rileyrohloff.my.workfront.com/attask/api/v10.0/logout?"
+create_task_url ="https://rileyrohloff.my.workfront.com/attask/api/v10.0/task?"
+task_url = "https://rileyrohloff.my.workfront.com/attask/api/v10.0/task/"
