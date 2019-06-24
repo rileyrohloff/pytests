8eee4dcf86e44ff5f3c3ac97fde46ae34458b2d1
rileyrohloff <rileyrohloff@workfront.com>
2019-06-24 16:54:16 -0600

    added the updating of the test task as part of the test

--- test_classes.py
+++ test_classes.py
@@ -7,6 +7,15 @@ class TestApi(unittest.TestCase):
     
     def test_firstUser(self):
         self.result = api_riley.create_session(user_urls.login_url)
-        self.projectCheck = api_riley.create_proj('PythonTests', api_riley.sessionID)
         self.assertEqual(self.result, 200)
+
+    def test_project(self):
+        self.projectCheck = api_riley.create_proj('PythonTests', api_riley.sessionID)
         self.assertEqual(self.projectCheck, 'PythonTests')
+
+    def test_task(self):
+        self.create_Task = api_riley.create_task('API health', api_riley.sessionID, api_riley.proj_ID)
+        self.assertEqual(self.create_Task, 'API health')
+    def test_updateTask(self):
+        self.updateTask = api_riley.update_task(api_riley.taskID, api_riley.sessionID)
+        self.assertEqual(self.updateTask, 'CPL')
