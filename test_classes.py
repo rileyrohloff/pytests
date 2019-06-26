cbff38ba83ff32b77ad2f6f3eeeaf5c4d577f9bb
rileyrohloff <rileyrohloff@workfront.com>
2019-06-26 16:29:50 -0600

    added the rest of the object tests in test_classes.py

--- test_classes.py
+++ test_classes.py
@@ -16,6 +16,16 @@ class TestApi(unittest.TestCase):
     def test_task(self):
         self.create_Task = api_riley.create_task('API health', api_riley.sessionID, api_riley.proj_ID)
         self.assertEqual(self.create_Task, 'API health')
+
     def test_updateTask(self):
         self.updateTask = api_riley.update_task(api_riley.taskID, api_riley.sessionID)
         self.assertEqual(self.updateTask, 'CPL')
+# Not sure why this is the case, however it looks like the pytest goes in alphabetical order. Hence why my last test is called zeleteObjects. If it is 'deleteObjects' the test runs first and fails.
+
+    def test_zeleteObjects(self):
+        self.deleteObjects = api_riley.tear_down(api_riley.proj_ID, api_riley.sessionID)
+        self.assertEqual(self.deleteObjects, 200)
+
+    def test_zz_closeTests(self):
+        self.closeTesting = api_riley.closeTest(api_riley.sessionID, user_urls.logout_url)
+        self.assertEqual(self.closeTesting, {'success': True})
